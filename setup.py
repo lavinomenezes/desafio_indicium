9367adf2e52ce704fac390a72ca82ea8a65d2178
Lavino Menezes <lprogramacao@protonmail.com>
2023-02-02 09:45:03 -0300

    first_commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='identificar quais máquinas apresentam potencial de falha',
+    author='Lavino_Menezes',
+    license='',
+)
